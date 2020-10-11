7a26a11f4cba546e67abf345723c3e76fe82617f
Sean Cone <seandoesitbetter@gmail.com>
2020-10-10 18:48:51 -0700

    Update SearchBooks.js

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -69,12 +69,14 @@ const SearchBooks = () => {
     }
 ///
     try {
-      const response = await saveBook(bookToSave, token);
+      const { data } = await saveBook({
+        variables: { bookToSave: bookToSave },
+      });
 
-      if (!response.ok) {
+      if (error) {
         throw new Error('something went wrong!');
       }
-
+      console.log('data re bookToSave', data);
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
