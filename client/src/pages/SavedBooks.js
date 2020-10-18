ef090513c6ed37d216a2442d5e2760eabe1478c6
Sean Cone <seandoesitbetter@gmail.com>
2020-10-18 12:27:37 -0700

    Fixing GET_ME

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -50,7 +50,7 @@ const SavedBooks = () => {
             : 'You have no saved books!'}
         </h2>
         <CardColumns>
-          {userData.savedBooks.map((book) => {
+          {userData.savedBooks?.map((book) => {
             return (
               <Card key={book.bookId} border="dark">
                 {book.image ? (
